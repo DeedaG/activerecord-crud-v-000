b0452206255d4ac8ef0edae9a56dae1f44406c95
Christia T <christiatidwell@bellsouth.net>
2018-08-19 19:22:24 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -88,8 +88,7 @@ def can_update_multiple_items_at_once
 end
 
 def can_destroy_a_single_item
-  movie = Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie.destroy
+  Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   __
 end
 
